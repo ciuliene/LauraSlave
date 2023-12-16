6d126733b6e7d55d94ee95ada8c909a7544c6032
ciuliene <errgioul2@gmail.com>
2023-12-16 17:51:05 +0100

    Game completed

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 let balls = [];
 let topScore = 0;
 let score = 0;
+let timeLeft = 0;
 let speed = 60;
+let button;
+let isTopScore = false;
+
+function startGame() {
+  balls = [];
+  score = 0;
+  timeLeft = 60;
+  speed = 60;
+  isTopScore = false;
+  button.remove();
+}
+
+function drawButton() {
+  button = createButton('INIZIA NUOVO GIOCO');
+  button.size(120, 45);
+  button.position(30, 105);
+  button.mousePressed(startGame);
+}
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  drawButton();
 }
 
 function draw() {
   background(0);
+
+  fill(255);
+  textSize(25);
+  text(`Score: ${score}`, 20, 40);
+
+  fill(`${isTopScore ? 'green' : 'red'}`);
+  textSize(16);
+  text(`Top Score: ${topScore}`, 20, 60);
+
+  fill(255);
+  textSize(25);
+  text(`Time left: ${timeLeft}`, 20, 90);
+
+  if (timeLeft <= 0) return;
 
   for (let ball of balls) {
     ball.draw();
@@ -20,6 +54,19 @@ function draw() {
 
   if (frameCount % speed == 0) {
     balls.push(new Ball());
+  }
+
+  if (frameCount % 60 === 0) {
+    timeLeft--;
+
+    if (timeLeft <= 0) {
+      drawButton();
+    }
+
+    speed -= 1.5;
+    if (speed < 1) {
+      speed = 1;
+    }
   }
 }
 
@@ -43,5 +90,6 @@ function mouseClicked() {
 }
 
 function touchStarted() {
+  if (timeLeft <= 0) return;
   return onPress();
 }
